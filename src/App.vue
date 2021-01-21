6772fb79050676570ff480a9b3ee2e481a93b8f5
morganfula <morgan.fula@me.com>
2021-01-21 19:16:26 +0100

    style done

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,18 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search..." />
       </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Lausanne, CH</div>
+          <div class="date">Monday 20 January 2021</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">9°c</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -20,6 +32,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;900&display=swap');
+
 * {
   margin: 0;
   padding: 0;
@@ -73,6 +87,50 @@ main {
       box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
       background-color: rgba(255, 255, 255, 0.75);
     }
+  }
+}
+
+.location-box {
+  .location {
+    color: #fff;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+    text-shadow: 1px 3px 16px rgba(0, 0, 0, 0.25);
+  }
+
+  .date {
+    color: #fff;
+    font-size: 20px;
+    font-weight: 300;
+    font-style: italic;
+    text-align: center;
+  }
+}
+
+.weather-box {
+  text-align: center;
+
+  .temp {
+    display: inline-block;
+    padding: 10px 25px;
+    color: #fff;
+    font-size: 102px;
+    font-weight: 900;
+
+    text-shadow: 3px 6px 12px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.25);
+    border-radius: 16px;
+    margin: 30px 0px;
+    box-shadow: 3px 6px 8px rgba(0, 0, 0, 0.25);
+  }
+
+  .weather {
+    color: #fff;
+    font-size: 48px;
+    font-weight: 700;
+    font-style: italic;
+    text-shadow: 3px 6px 12px rgba(0, 0, 0, 0.25);
   }
 }
 </style>
